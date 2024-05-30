8c5c3b0076b0a149885d015a5a3b0c6679f9b3dc
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2024-05-30 19:23:36 +0300

    modification fonction peuplerBDavecForkJoin

--- src/app/shared/assignments.service.ts
+++ src/app/shared/assignments.service.ts
@@ -110,6 +110,10 @@ export class AssignmentsService {
       nouvelAssignment.nom = a.nom;
       nouvelAssignment.dateDeRendu = new Date(a.dateDeRendu);
       nouvelAssignment.rendu = a.rendu;
+      nouvelAssignment.auteur = a.auteur;
+      nouvelAssignment.matiere = a.matiere;
+      nouvelAssignment.note = a.note as number;
+      nouvelAssignment.remarques = a.remarques as string;
 
       this.addAssignment(nouvelAssignment)
         .subscribe(() => {
